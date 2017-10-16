f7b10a2b77b4c06184b596829c1fcdaceb80f66b
Leonar Gharib <leonargharib@gmail.com>
2017-10-16 02:30:06 -0700

    heroCounters.py calculates win rates of heroes vs other heroes

---
+++ heroCounters.py
@@ -0,0 +1,149 @@
+import json
+import requests
+from datetime import date
+from dateutil.relativedelta import relativedelta
+from  pprint import pprint
+
+# used for enemy heroes
+class Hero:
+    wins = 0.0
+    losses = 0.0
+    name = ''
+    def __init__(self,name):
+        self.name = name
+    def incWin(self):
+        self.wins += 1
+    def incLoss(self):
+        self.losses += 1
+    def getTotal(self):
+        return self.wins+self.losses
+    def getWinPerc(self):
+        return self.wins/self.getTotal()
+    def getLossPerc(self):
+        return self.losses/self.getTotal()
+    def getName(self):
+        return self.name
+    def getWins(self):
+        return int(self.wins)
+    def getLosses(self):
+        return int(self.losses)
+class WinLoss:
+    enemyHeroes = {}
+    # creates new Hero object and adds to list
+    def addHero(self, name):
+        newHero = Hero(name)
+        self.enemyHeroes[name] = newHero
+    def incWin(self,heroName):
+        if self.enemyHeroes == False:
+            self.addHero(heroName)
+        if (heroName in self.enemyHeroes) == False:
+            self.addHero(heroName)
+        self.enemyHeroes[heroName].incWin()
+    def incLoss(self,heroName):
+        if self.enemyHeroes == False:
+            self.addHero(heroName)
+        if (heroName in self.enemyHeroes) == False:
+            self.addHero(heroName)
+        self.enemyHeroes[heroName].incLoss()
+    def getWinPerc(self,heroName):
+        if self.enemyHeroes == False:
+            self.addHero(heroName)
+        if (heroName in self.enemyHeroes) == False:
+            self.addHero(heroName)
+        self.enemyHeroes[heroName].getWinPerc()
+    def getHeroes(self):
+        return self.enemyHeroes
+
+#calculates date 1 month ago
+curr_page = 1
+prev_month = date.today() + relativedelta(months=-1)
+correct_day = prev_month.strftime('%Y-%m-%d')
+game_type = "HeroLeague"
+
+#print "Requesting replays" #from http://hotsapi.net/api/v1/replays/paged?page="+str(curr_page)+"&start_date="+correct_day+"&game_type="+game_type
+response = requests.get("http://hotsapi.net/api/v1/replays/paged?page="+str(curr_page)+"&start_date="+correct_day+"&game_type="+game_type)
+#print "Finished requesting"
+
+data = json.loads(response.text)
+
+heroDict = {}
+
+lenReplays = len(data['replays'])
+
+for x in range(0,lenReplays):
+    #print "Checking replay " + str(x+1)
+    currID =  data['replays'.decode('utf-8')][x]['id']
+
+    #print("got id:"+str(currID))
+
+    # want to count hero popularity
+    response = requests.get('http://hotsapi.net/api/v1/replays/'+str(currID))
+
+    # loads data of certain replay ( got through ID )
+    replayData = json.loads(response.text)
+
+    arrLen = len(replayData['players'])
+
+    numWinners = 0
+    numLosers = 0
+    winners = [''] * 5
+    losers = [''] * 5
+
+    # iterate through heroes played in certain match, keeping count
+    for i in range (0,arrLen):
+        heroName = (replayData['players'][i]['hero']).encode("utf-8")
+
+        # sort winners & losers into 2 arrays
+        if replayData['players'][i]['winner'] == True:
+            winners[numWinners] = (replayData['players'][i]['hero']).encode("utf-8")
+            numWinners += 1
+        else:
+            losers[numLosers] = (replayData['players'][i]['hero']).encode("utf-8")
+            numLosers += 1
+
+    # adds wins and losses to heroDict
+    for win in range (0, 5):
+        winner = winners[win]
+        for lose in range (0,5):
+            loser = losers[lose]
+
+            # heroDict['Stitches'] = winLoss
+            # winLoss is WinLoss obj
+
+
+            # ex heroDict['Stitches']['Chromie']['win'] += 1
+            if (winner in heroDict) == False:
+                print "updating heroDict with winners key: " + winner
+                winLoss = WinLoss()
+                heroDict[winner] = winLoss
+
+            heroDict[winner].incWin(loser)
+
+            if (loser in heroDict) == False:
+                winLoss = WinLoss()
+                heroDict[loser] = winLoss
+
+            heroDict[loser].incLoss(winner)
+
+lenDict = len(heroDict)
+
+file = open("stats.txt","w")
+
+for hero in heroDict:
+    enemyHeroes = heroDict[hero].getHeroes()
+    for enemyHero in enemyHeroes:
+        '''
+        file.write(hero + "" + str(enemyHeroes[enemyHero].getWinPerc()*100)
+            + " win rate vs " + enemyHeroes[enemyHero].getName() + " out of " +
+            str(enemyHeroes[enemyHero].getTotal()) + " games")
+        '''
+        perclen = len(str(round(enemyHeroes[enemyHero].getWinPerc()*100,3)))
+        file.write(hero + " " * (18-len(hero)) +
+                str(round(enemyHeroes[enemyHero].getWinPerc()*100,3)) +
+                " " * (8-perclen) + enemyHeroes[enemyHero].getName() +
+                " " * (18-len(enemyHeroes[enemyHero].getName())) +
+                " " * 5 + str(int(enemyHeroes[enemyHero].getTotal())) +
+                " " * 5 + str(int(enemyHeroes[enemyHero].getWins())) +
+                " " * 5 + str(int(enemyHeroes[enemyHero].getLosses())) + "\n")
+
+file.close()
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import requests
 from datetime import date
 from dateutil.relativedelta import relativedelta
+from  pprint import pprint
 
 #calculates date 1 month ago
 curr_page = 1
@@ -11,9 +12,43 @@ correct_day = prev_month.strftime('%Y-%m-%d')
 response = requests.get("http://hotsapi.net/api/v1/replays/paged?page="+str(curr_page)+"&start_date="+correct_day)
 
 #rj = response.json()
+#pprint(rj)
 data = json.loads(response.text)
 
-print data['replays'][0]['id']
+heroDict = {}
 
-#want to find most popular heroes
+lenReplays = len(data['replays'])
+print "type of data is: " + str(data.__class__)
+print "lenReplays = "+ str(lenReplays)
+
+lenReplays = 10
+# loop over all replays
+for x in range(0,lenReplays):
+
+    print "Running loop iteration: " + str(x)
+
+    currID =  data['replays'.decode('utf-8')][x]['id']
+
+    print("got id: "+str(currID))
+
+    # want to count hero popularity
+    # loads data of certain replay ( got through ID )
+    response = requests.get('http://hotsapi.net/api/v1/replays/'+str(currID))
+    replayData = json.loads(response.text)
+
+    arrLen = len(replayData['players'])
+
+    # iterate through heroes played in certain match, keeping count
+    for i in range (0,arrLen):
+        heroName = (replayData['players'][i]['hero']).encode("utf-8")
+        if heroName in heroDict:
+            heroDict[heroName] += 1
+        else:
+            heroDict[heroName] = 1
+
+lenDict = len(heroDict)
+
+for key in heroDict:
+    print "key: %s, value: %s" % (key, heroDict[key])
+
 
